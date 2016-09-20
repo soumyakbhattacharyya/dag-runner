811b9f0305b591e3abff52ad2f3cbfccef188b99
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2016-09-20 22:42:12 +0530

    introduced required package structure

---
+++ src/main/java/org/sb/core/FunctionalSpecification.java
@@ -0,0 +1,18 @@
+package org.sb.core;
+
+public interface FunctionalSpecification {
+
+  //@formatter:off
+  
+  /*
+   * acceptance criteria 
+   *  
+   * 1) utility will be easy to extend, simple to understand and performant
+   * 2) it will be possible to define tasks which would execute depending on completion status of other tasks 
+   * 3) tasks will themselves follow commons chain paradigm (signature etc.)
+   * 4) it will be easy to document dependencies between tasks using json
+   *  
+   */
+   
+  //@formatter:on
+}
--- src/main/java/org/sb/core/Main.java
+++ src/main/java/org/sb/core/Main.java
@@ -1,5 +1,9 @@
 package org.sb.core;
 
 public class Main {
+  
+  /**
+   *  
+   */
 
 }
